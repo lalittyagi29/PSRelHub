93608c165111074abfcf491ff1ffa9d51cd3bd60
lalittyagii <tyagilalit661@gmail.com>
2022-09-21 17:20:17 +0530

    gelocation  file

--- src/main/java/com/ps/payload/GeoLocation.java
+++ src/main/java/com/ps/payload/GeoLocation.java
@@ -2,9 +2,34 @@ package com.ps.payload;
 
 public class GeoLocation {
 
-    private double latitude;
-    private double longitude;
+	private double latitude;
+	private double longitude;
 
-    
-    
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public GeoLocation(double latitude, double longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public GeoLocation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
 }
